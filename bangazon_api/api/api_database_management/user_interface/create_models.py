e66e31174a69ed1120008801839607590c23e414
Taylor Perkinse <perkinstaylor.luthier@gmail.com>
2017-04-21 16:02:33 -0500

    Made adjustents to doctsring

--- bangazon_api/api/api_database_management/user_interface/create_models.py
+++ bangazon_api/api/api_database_management/user_interface/create_models.py
@@ -2,13 +2,14 @@ from django.db import models
 
 class Order(models.Model):
     """This class defines what bangazon's Order table within the database will look like
+        You need both PaymentType and Customer table for this to work correctly
 
     Attributes:
         PaymentTypeId (INT): Payment Type Id referencing table (FK)
         CustomerId (INT): Customer Id referencing table (FK)
     """
-    PaymentTypeId = models.ForeignKey(PaymentTypes)    
-    CustomerId = models.ForeignKey(Customers)    
+    PaymentTypeId = models.ForeignKey(PaymentType)    
+    CustomerId = models.ForeignKey(Customer)    
 
 
 
