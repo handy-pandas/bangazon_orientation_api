9393d9802f6e3667f1065e69a56fd1df1c48747d
Talbot <talbotlawrence75@gmail.com>
2017-04-21 15:22:09 -0500

    walking through the models design

--- bangazon_api/api/serializers.py
+++ bangazon_api/api/serializers.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import serializers
+from api.models import *
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
