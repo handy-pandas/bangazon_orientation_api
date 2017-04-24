015b9fc513fe5c9abad1edfc179b2f489b1882b3
James Tonkin <jameswtonkin@gmail.com>
2017-04-24 15:14:23 -0500

    added classes to expose compter to api

--- bangazon_api/settings.py
+++ bangazon_api/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'django.contrib.admin',
+    'bangazon_api.api',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
