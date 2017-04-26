f7b38ba59af72566376c7780546e9cd253d7fb0e
“Aaron <barfootaaron@yahoo.com>
2017-04-26 13:50:30 -0500

    fixed variable names in docstrings

--- bangazon_api/api/models.py
+++ bangazon_api/api/models.py
@@ -49,8 +49,8 @@ class Order(models.Model):
     """This class defines what bangazon's Order table within the database will look like
         You need both PaymentType and Customer table for this to work correctly
     Attributes:
-        PaymentTypeId (INT): Payment Type Id referencing Payment Type table (FK)
-        CustomerId (INT): Customer Id referencing Customer table (FK)
+        payment_type (INT): Payment Type Id referencing Payment Type table (FK)
+        customer (INT): Customer Id referencing Customer table (FK)
     Author: Taylor Perkins
     """
     payment_type = models.ForeignKey(PaymentType)
@@ -60,8 +60,8 @@ class ProductOrder(models.Model):
     """This class is a join table for both the Product and Order Table within Bangazon db
         You will need both the Product and Order tables created for this table to apply
     Attributes:
-        ProductId (INT): Product Id referencing Product table (FK)
-        OrderId (INT): Order Id referencing Order table (FK)
+        product (INT): Product Id referencing Product table (FK)
+        order (INT): Order Id referencing Order table (FK)
     Author: Taylor Perkins
     """
 
@@ -74,8 +74,8 @@ class Department(models.Model):
     Class for adding product table to the database
 
 
-    Name = models.CharField(max_length=55)
-    Budget = models.DecimalField(max_digits=6, decimal_places=2)
+    name = models.CharField(max_length=55)
+    budget = models.DecimalField(max_digits=6, decimal_places=2)
 
     Author:
     wocaldwell
@@ -87,7 +87,7 @@ class Employee(models.Model):
     """Class designed for creating employees within database
 
     Attributes:
-    DepartmentId (INT): Department Id referencing the Department table (FK)
+    department (INT): Department Id referencing the Department table (FK)
 
     Author: Talbot Lawrence
     """
